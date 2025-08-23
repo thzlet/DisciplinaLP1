a2576852ad6669a3b07fcabe480f1921046d830c
thzlet <leticiarosembergsousa@gmail.com>
2025-08-23 19:38:27 -0300

    Aula 6

--- AP1N1/TAD_LISTA_HETEROGENEA/TAD_LISTA_HETEROGENA.h
+++ AP1N1/TAD_LISTA_HETEROGENEA/TAD_LISTA_HETEROGENA.h
@@ -1,9 +1,14 @@
 #ifndef __TAD_LISTA_HETEROGENEA__
     #define __TAD_LISTA_HETEROGENEA__
+    
+    #include <stdlib.h>
+    #include <stdio.h>
 
     #define RET 0
     #define TRI 1
     #define CIR 2
+
+    #define PI 3.14F
 
     struct lista_het{
         char tipo;
@@ -33,17 +38,21 @@
     Retangulo* cria_ret(float b, float h);
     Triangulo* cria_tri(float b, float h);
     Circulo* cria_cir(float r);
+
     Lista_het* lista_het_cria();
     Lista_het* lista_het_insere(Lista_het* lista, void* info, char tipo);
     int lista_het_vazia(Lista_het* lista);
     void lista_het_libera(Lista_het* lista);
+    
     Retangulo* lista_het_recupera_ret(Lista_het* lista);
     Triangulo* lista_het_recupera_tri(Lista_het* lista);
     Circulo* lista_het_recupera_cir(Lista_het* lista);
+
     char lista_het_tipo_primeiro_elemento(Lista_het* lista);
     float area(Lista_het* elemento);
     float max_area(Lista_het* lista);
 
+    void lista_het_imprime(Lista_het* lista);
 
-    #include "TAD_LISTA_HETEROGENEA.h"
+    #include "TAD_LISTA_HETEROGENEA.c"
 #endif
